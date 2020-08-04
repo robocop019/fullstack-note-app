e565749859663a3a19040aae331814adb347dc95
Chris Bowen <chrisbowen@comcast.net>
2020-08-04 15:41:03 -0500

    added frontend production build

---
+++ build/precache-manifest.44254e2513fa294dbd8166d514e3e42f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7306aac0156c50c39d0cb54d24668ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078c71acdacfcde48790",
+    "url": "/static/css/main.ea47ea8b.chunk.css"
+  },
+  {
+    "revision": "d7fc3c20b08961749f5a",
+    "url": "/static/js/2.613c2fce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.613c2fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078c71acdacfcde48790",
+    "url": "/static/js/main.a44f78d3.chunk.js"
+  },
+  {
+    "revision": "048e03eeae34ec21bb0c",
+    "url": "/static/js/runtime-main.be59e513.js"
+  }
+]);
